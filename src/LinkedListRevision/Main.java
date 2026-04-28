bc91732f0d0b547b05d52c4e749c8f61516794a2
Mohammad Shabbir <mdshabbirak47@gmail.com>
2026-04-28 22:45:36 +0530

    new linkedlist file added

--- src/LinkedListRevision/Main.java
+++ src/LinkedListRevision/Main.java
@@ -6,10 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Node head = convertToLL(arr);
 //        printlLL(head);
-        System.out.println("The length of the linkedList is: " + lengthOfLL(head));
-        System.out.println("1 is present in the linkedlist or not: "  + isPresent(head,64));
-
+        Node head1 = deletKthNode(head,5);
+        printlLL(head1);
+//        printlLL(tail);
         // deletetion and insertion in linked list:
+
+        // delete the Kth node of the LinkedList
+
 
     }
     public static Node convertToLL(int[] arr) {
@@ -27,6 +30,16 @@ public class Main {
             head = head.next;
         }
     }
+    public static int sizeOfLL(Node head) {
+        Node temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+
+        }
+        return  count;
+    }
     public static int lengthOfLL(Node head) {
         int len = 0;
         while (head != null) {
@@ -43,10 +56,41 @@ public class Main {
         }
         return  false;
     }
+    // delete head
+    public static  Node deleteHead(Node head) {
+        Node temp = head;
+        return  temp.next;
+    }
+    // delete tail
+    public static Node deleteTail(Node head) {
+        Node temp = head;
+        while (temp!= null) {
+            if(temp.next.next == null) {
+              temp.next = null;
+                break;
+            }
+            temp = temp.next;
+        }
+        return  head;
+    }
+
+    public static Node deletKthNode(Node head, int pos) {
+        Node temp = head;
+        for (int i = 1; i < sizeOfLL(head); i++) {
+            if(i == pos-1) {
+               temp.next = temp.next.next;
+               continue;
+            }
+            temp = temp.next;
+        }
+        return  head;
+    }
+
 }
 class Node {
     int data;
     Node next;
+    Node prev;
     Node(int data1 , Node next1) {
         this.data = data1;
         this.next = next1;
